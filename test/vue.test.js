0ff9a96dbb78ba5ce28e634123320861087f108d
Gorka <gorka.d3v@gmail.com>
2023-08-16 20:47:37 +0200

    Configure testing enviroment

---
+++ test/vue.test.js
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest';
+import { render, fireEvent, screen} from '@testing-library/vue'
+import Component from '../src/components/Basic.vue'
+
+test('increments value on click', async () => {
+  render(Component)
+
+  screen.getByText('Times clicked: 0')
+
+  const button = screen.getByText('increment')
+
+  await fireEvent.click(button)
+  await fireEvent.click(button)
+
+  screen.getByText('Times clicked: 2')
+})
--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,12 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
+import vue from '@vitejs/plugin-vue';
 
 export default getViteConfig({
 	test: {
+		environment: 'happy-dom'
 		/* for example, use global to avoid globals imports (describe, test, expect): */
 		// globals: true,
 	},
+	plugins: [vue()]
 });
